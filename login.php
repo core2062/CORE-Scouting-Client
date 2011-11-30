b3f43a15d1b48a3138ccd6809839b2779c1f8dda
slang800@gmail.com <slang800@gmail.com>
2011-11-29 21:42:02 -0600

    removed table formatter

--- login.php
+++ login.php
@@ -1,5 +1,17 @@
 <?php
+ // remove this section before production
+require_once('FirePHP/fb.php');
+ob_start();
+
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+?>
+
+<?php
 // TODO finish token based login
+
+$m = new Mongo(); // connect
+$db = $m->selectDB("user");
 
 $token = uniqid("",true);
 fb($token);
