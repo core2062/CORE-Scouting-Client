561fe1f7229f4c7a66c802a0b5525d655d3097e5
Sean Lang <slang800@gmail.com>
2011-12-01 19:56:51 -0600

    finished token-gen mechanisim

--- login.php
+++ login.php
@@ -8,19 +8,24 @@ ini_set( 'display_errors', 1 );
 ?>
 
 <?php
-// TODO finish token based login
+//start timer
+list($micro, $sec) = explode(" ",microtime());
+$starttime = (float)$sec + (float)$micro;
+
+//start log var
+$log = "";
+
+$m = new Mongo(); // connect
+$db = $m->selectDB("CSD");
 
 //variables_order must contain "C" in php.ini
-$scoutid = $_COOKIE['scoutid'] or die("error");
-$pword_input = $_COOKIE['pword'] or die("error");
-$ip=$_SERVER['REMOTE_ADDR'] or die("error");
+$scoutid = $_COOKIE['scoutid'] or send_error("scoutid not in cookie","");
+$pword_input = $_COOKIE['pword'] or send_error("pword not in cookie","");
+$ip=$_SERVER['REMOTE_ADDR'] or send_error("cannot get ip","");
 
 fb($scoutid);
 fb($pword_input);
 fb($ip);
-
-$m = new Mongo(); // connect
-$db = $m->selectDB("CSD");
 
 $pword = $db->execute("db.user.findOne({_id : '$scoutid'},{_id:0, pword:1})");
 $pword = $pword["retval"]["pword"];
@@ -32,40 +37,38 @@ if ($pword !== $pword_input) {
 	//return bad pword
 }
 
-
-fb($pword);
-
 $token = uniqid("",true);
-fb($token);
 
-// db.user.find({scoutid : "SeanLang-2062"},{_id:0, email:1, permission:1})
-//$response = $db->execute("function(greeting, name) { return greeting+', '+name+'!'; }", array("Good bye", "Joe"));
-//echo $response['retval'];
+//regular end
+list($micro, $sec) = explode(" ",microtime());
+$endtime = (float)$sec + (float)$micro;
+$total_time = ($endtime - $starttime);
 
+$input = "{type:'token-gen', scoutid:'$scoutid', time:'$starttime', duration:'$total_time', place:'login.php', token:'$token', ip:'$ip', log:'$log'}";
+$db->execute("db.log.insert($input)");
+
+ob_clean (); //empty output buffer, error_text is only thing sent
+
+die("{token:'$token'}");
 
 function send_error($error_text, $error) {
+	global $db;
 	global $starttime;
 	global $log;
 	global $scoutid;
-	
-	$log.= $error;
-	if ($error == "") {$log.= " | " . $error_text;}
+
+	if ($error == "") {$error = $error_text;}
 	
 	list($micro, $sec) = explode(" ",microtime());
 	$endtime = (float)$sec + (float)$micro;
 	$total_time = ($endtime - $starttime);
+	
+	$input = "{type:'error', scoutid:'$scoutid', time:'$starttime', duration:'$total_time', place:'login.php', errorcode:'$error', ip:'$ip', log:'$log'}";
+	$db->execute("db.log.insert($input)");
+	
+
 	
 	ob_clean (); //empty output buffer, error_text is only thing sent
-
-}
-function send_reg() {
-	global $starttime;
-	global $log;
-	global $scoutid;
-	
-	list($micro, $sec) = explode(" ",microtime());
-	$endtime = (float)$sec + (float)$micro;
-	$total_time = ($endtime - $starttime);
-	
+	die("{error:'$error_text'}");
 }
 ?>
