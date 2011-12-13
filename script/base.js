0d1de6959a7cf652d246ed53b4f8fabdc49552a7
slang800@gmail.com <slang800@gmail.com>
2011-12-12 21:50:50 -0600

    starting work on login function

--- script/base.js
+++ script/base.js
@@ -38,12 +38,10 @@ console.log('Hello and welcome to the CSD, a intuitive scouting database and ana
 
 //set fieldset class on focus
 $("input, textarea").focus(function () {
-	"use strict";
     $(this).parentsUntil($("form"), "fieldset").addClass('focus');
 });
 
 $("input, textarea").focusout(function () {
-	"use strict";
     $(this).parentsUntil($("form"), "fieldset").removeClass('focus');
 });
 
@@ -221,8 +219,25 @@ $(document).ready(function () {
 	cacheModals();
 	nav();
 	
-	window.scoutid = EatCookie('scoutid');
-	window.token = EatCookie('token');
+	if(eatCookie('user') !== '') {
+		window.user = eval(eatCookie('user'));
+	} else {
+		window.user = {
+			"scoutid": "",
+			"token": "",
+			"prefs": []
+		};
+	}
+	
+	window.user = {
+		"scoutid": [],
+		"token": []
+	};
+	
+	window.scoutid = eatCookie('scoutid');
+	window.token = eatCookie('token');
+	// TODO change to decoding  "user" object from cookie
+	
 	if (scoutid == '' || token == ''){
 		LoginCheck();
 	}
@@ -338,7 +353,7 @@ function nav() {
 				});
 			}
 		}
-	} else if(current.type == 'modals'){ //modal
+	} else { //modal
 		document.getElementById('modal-title').innerHTML = current.subpage;
 		
 		if (prev.type == 'subpages') { //subpages
@@ -358,28 +373,45 @@ function modalclose() {
 		window.location = '#' + current.lastSub;
 }
 
-function BakeCookie(name, value) {
+function bakeCookie(name, value) {
 	var expires = new Date();
 	expires.setTime(expires.getTime()+(15552000000));
 	document.cookie = name + "=" + value + "; expires=" + expires.toGMTString() + "; path=/";
 }
 
-function EatCookie(name) {
+function eatCookie(name) {
 	var nameEQ = name + "=";
 	var ca = document.cookie.split(';');
 	for(var i=0;i < ca.length;i++) {
 		var c = ca[i];
 		while (c.charAt(0)==' ') c = c.substring(1);
 		if (c.indexOf(nameEQ) == 0) {
-			var CookieValue = c.substring(nameEQ.length);
+			var cookieValue = c.substring(nameEQ.length);
 			break;
 		}
-		else var CookieValue = "";
+		else var cookieValue = "";
 	}
-	return CookieValue;
+	return cookieValue;
 }
 
-function login(){
+//TODO change login button to global user-button (id = userButton)
+//made as button set, name in one part (opens edit-account modal)
+//other part has logout (icon & tipsy)
+
+function loginCheck(){
+	if (eatCookie('scoutid') !== '' && eatCookie('token') !== '') {
+		//assume logged in, if token is wrong error will be returned by process later
+		//other wise there would be too many login checks
+		return;
+	}
+	
+	
+	//is login stuff filled out
+		//then attempt login
+	
+	//does page require login
+		//then open login modal & say page requires login
+	
 	scoutidinput = document.getElementById('scoutid');
 	pwordinput = document.getElementById('pword');
 	loginbutton = document.getElementById('login-button');
@@ -388,11 +420,7 @@ function login(){
 	pword = pwordinput.value;
 }
 
-function logout(){
-	
-}
-
-function getToken(password){ //TODO make login check run at login modal close
+function getToken(password){ //merge function with login
 	scoutidinput = document.getElementById('scoutid');
 	pwordinput = document.getElementById('pword');
 	loginbutton = document.getElementById('login-button');
@@ -408,19 +436,25 @@ function getToken(password){ //TODO make login check run at login modal close
 		var json = post('login.php','{"scoutid":"' + scoutid + '","pword":"' + pword + '"}');
 
 		if (json.token) {
-			BakeCookie('scoutid', scoutid);
-			BakeCookie('token', json.token);
+			// TODO store all user prefs in 'user' object
+			
+			bakeCookie('user','{"scoutid": "' + window.user.scoutid + '", "token": "' + window.user.scoutid + '",' + '}');
+			
+			bakeCookie('token', json.token);
+			
+			// TODO store JSON encoded user object in cookie
 			
 			loginbutton.innerHTML = 'Logout';
 			scoutidinput.value = '';
 			pwordinput.value = '';
+			
+			//store all user stuff to 'user object'
+			
 			return true
 		} else if (json.error) {
 			$('#jGrowl-container').jGrowl('Login Failure: ' + json.error, {theme: 'error'});
-			return false
 		} else {
 			$('#jGrowl-container').jGrowl('Login Failure: Server did not respond properly', {theme: 'error'});
-			return false
 		}
 	}
 	
@@ -432,7 +466,7 @@ function logout(){
 	scoutid = '';
 	token = '';
 	loginbutton.innerHTML = 'Login';
-	window.location = '#Login';
+	loginCheck();
 }
 
 
@@ -1030,7 +1064,7 @@ function post(filename, json) {
 // TODO replace with something better, like downloadify or just a modal
 function WriteToWindow() {
 top.consoleRef=window.open('','myconsole','width=350,height=250,menubar=0,toolbar=1,status=0,scrollbars=1,resizable=1');
-//TODO fix link to style sheet, or replace compleatly
+//TODO fix link to style sheet, or replace completely
 top.consoleRef.document.write(
 '<html><head><title>Scouting Data</title></head><body bgcolor=white onLoad="self.focus()"><textarea style="width:100%; height:100%;">'
 +writetext
@@ -1116,3 +1150,196 @@ case "Q": // Query
 	
 break;
 */
+/**
+ * jQuery JSON Plugin
+ * version: 2.3 (2011-09-17)
+ *
+ * This document is licensed as free software under the terms of the
+ * MIT License: http://www.opensource.org/licenses/mit-license.php
+ *
+ * Brantley Harris wrote this plugin. It is based somewhat on the JSON.org
+ * website's http://www.json.org/json2.js, which proclaims:
+ * "NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.", a sentiment that
+ * I uphold.
+ *
+ * It is also influenced heavily by MochiKit's serializeJSON, which is
+ * copyrighted 2005 by Bob Ippolito.
+ */
+
+(function( $ ) {
+
+	var	escapeable = /["\\\x00-\x1f\x7f-\x9f]/g,
+		meta = {
+			'\b': '\\b',
+			'\t': '\\t',
+			'\n': '\\n',
+			'\f': '\\f',
+			'\r': '\\r',
+			'"' : '\\"',
+			'\\': '\\\\'
+		};
+
+	/**
+	 * jQuery.toJSON
+	 * Converts the given argument into a JSON respresentation.
+	 *
+	 * @param o {Mixed} The json-serializble *thing* to be converted
+	 *
+	 * If an object has a toJSON prototype, that will be used to get the representation.
+	 * Non-integer/string keys are skipped in the object, as are keys that point to a
+	 * function.
+	 *
+	 */
+	$.toJSON = typeof JSON === 'object' && JSON.stringify
+		? JSON.stringify
+		: function( o ) {
+
+		if ( o === null ) {
+			return 'null';
+		}
+
+		var type = typeof o;
+
+		if ( type === 'undefined' ) {
+			return undefined;
+		}
+		if ( type === 'number' || type === 'boolean' ) {
+			return '' + o;
+		}
+		if ( type === 'string') {
+			return $.quoteString( o );
+		}
+		if ( type === 'object' ) {
+			if ( typeof o.toJSON === 'function' ) {
+				return $.toJSON( o.toJSON() );
+			}
+			if ( o.constructor === Date ) {
+				var	month = o.getUTCMonth() + 1,
+					day = o.getUTCDate(),
+					year = o.getUTCFullYear(),
+					hours = o.getUTCHours(),
+					minutes = o.getUTCMinutes(),
+					seconds = o.getUTCSeconds(),
+					milli = o.getUTCMilliseconds();
+
+				if ( month < 10 ) {
+					month = '0' + month;
+				}
+				if ( day < 10 ) {
+					day = '0' + day;
+				}
+				if ( hours < 10 ) {
+					hours = '0' + hours;
+				}
+				if ( minutes < 10 ) {
+					minutes = '0' + minutes;
+				}
+				if ( seconds < 10 ) {
+					seconds = '0' + seconds;
+				}
+				if ( milli < 100 ) {
+					milli = '0' + milli;
+				}
+				if ( milli < 10 ) {
+					milli = '0' + milli;
+				}
+				return '"' + year + '-' + month + '-' + day + 'T' +
+					hours + ':' + minutes + ':' + seconds +
+					'.' + milli + 'Z"';
+			}
+			if ( o.constructor === Array ) {
+				var ret = [];
+				for ( var i = 0; i < o.length; i++ ) {
+					ret.push( $.toJSON( o[i] ) || 'null' );
+				}
+				return '[' + ret.join(',') + ']';
+			}
+			var	name,
+				val,
+				pairs = [];
+			for ( var k in o ) {
+				type = typeof k;
+				if ( type === 'number' ) {
+					name = '"' + k + '"';
+				} else if (type === 'string') {
+					name = $.quoteString(k);
+				} else {
+					// Keys must be numerical or string. Skip others
+					continue;
+				}
+				type = typeof o[k];
+
+				if ( type === 'function' || type === 'undefined' ) {
+					// Invalid values like these return undefined
+					// from toJSON, however those object members
+					// shouldn't be included in the JSON string at all.
+					continue;
+				}
+				val = $.toJSON( o[k] );
+				pairs.push( name + ':' + val );
+			}
+			return '{' + pairs.join( ',' ) + '}';
+		}
+	};
+
+	/**
+	 * jQuery.evalJSON
+	 * Evaluates a given piece of json source.
+	 *
+	 * @param src {String}
+	 */
+	$.evalJSON = typeof JSON === 'object' && JSON.parse
+		? JSON.parse
+		: function( src ) {
+		return eval('(' + src + ')');
+	};
+
+	/**
+	 * jQuery.secureEvalJSON
+	 * Evals JSON in a way that is *more* secure.
+	 *
+	 * @param src {String}
+	 */
+	$.secureEvalJSON = typeof JSON === 'object' && JSON.parse
+		? JSON.parse
+		: function( src ) {
+
+		var filtered = 
+			src
+			.replace( /\\["\\\/bfnrtu]/g, '@' )
+			.replace( /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+			.replace( /(?:^|:|,)(?:\s*\[)+/g, '');
+
+		if ( /^[\],:{}\s]*$/.test( filtered ) ) {
+			return eval( '(' + src + ')' );
+		} else {
+			throw new SyntaxError( 'Error parsing JSON, source is not valid.' );
+		}
+	};
+
+	/**
+	 * jQuery.quoteString
+	 * Returns a string-repr of a string, escaping quotes intelligently.
+	 * Mostly a support function for toJSON.
+	 * Examples:
+	 * >>> jQuery.quoteString('apple')
+	 * "apple"
+	 *
+	 * >>> jQuery.quoteString('"Where are we going?", she asked.')
+	 * "\"Where are we going?\", she asked."
+	 */
+	$.quoteString = function( string ) {
+		if ( string.match( escapeable ) ) {
+			return '"' + string.replace( escapeable, function( a ) {
+				var c = meta[a];
+				if ( typeof c === 'string' ) {
+					return c;
+				}
+				c = a.charCodeAt();
+				return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
+			}) + '"';
+		}
+		return '"' + string + '"';
+	};
+
+})( jQuery );
