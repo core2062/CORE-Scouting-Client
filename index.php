43372b13fcc63c3ba8a41e1c48d3abe5d93c3f6f
Sean Lang <slang800@gmail.com>
2012-01-14 11:05:56 -0600

    about to cut android password input out... just not working (THIS MIGHT BE A IMPORTANT HISTORY POINT)

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ ini_set( 'display_errors', 1 );
 <!-- TODO make a kill IE 6 page like http://themeforest.net/item/kill-ie6-template/full_screen_preview/795628 -->
 <!-- TODO add gzip to server -->
 <!-- TODO make sure links on facebook contain a nice description of the site -->
-<!-- TODO make signup page hidded when logged in -->
+<!-- TODO make signup page hidden when logged in -->
 <!-- TODO make onload scripts for each page -->
 <!-- TODO make a small dropdown for the nav button that gives the page catagories -->
 
