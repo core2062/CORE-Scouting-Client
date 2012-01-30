87dad7e7ffc32447180924217f691fef4e678be2
Sean Lang <slang800@gmail.com>
2012-01-30 15:55:58 -0600

    work on signup

--- signup.php
+++ signup.php
@@ -19,13 +19,23 @@ $input = json_decode($input, true);
 //primary error correction is done client-side, error messages from this are not friendly
 
 //check for missing required data (for optional, assign default)
-$required = array('fName', 'lName');
+
+//array_keys()    array array_keys ( array $input [, mixed $search_value [, bool $strict = false ]] )
+//array_merge_recursive()     array array_merge_recursive ( array $array1 [, array $... ] )
+
+$objects = array('info', 'account');
+
+$required = array(
+	array('info', 'fName'),
+	array('info', 'lName'),
+	array('info', 'email')
+);
 
 $len = sizeof($required);
 for(var $i = 0; i < $len; $i++){
-    if (empty($input[$required[$i]]) == true){
-        send_error('something wasn\'t not sent');
-    }
+	if (empty($input[ $required[$i][1] ][ $required[$i][2] ]) == true){
+		send_error($required[$i] . ' wasn\'t not sent');
+	}
 }
 
 
@@ -40,35 +50,43 @@ $db->execute("
 	db.user.insert(" . json_encode($input) . ");
 ");
 
-//token and logintime are assigned at 1st login
-
- /*
-{
-   "_id": "SeanLang-2062",
-   "email": "slang800@gmail.com",
-   "info": {
-     "fName": "Sean",
-     "lName": "Lang",
-     "zip": 53072,
-     "browser": "Firefox"
-  },
-   "ip": "127.0.0.1",
-   "logintime": "1327269383.167",
-   "permission": 9,
-   "prefs": {
-     "fade": true,
-     "verbose": true
-  },
-   "pword": "superpass",
-   "stats": {
-     "gender": "m"
-  },
-   "team": 2062,
-   "token": "4f1c860728df71.38499022"
-}
-
- */
-
 //send confirmation email + instructions for training
 
+/*
+
+The User Object (full example)
+{
+	"_id": "SeanLang-2062",
+	"permission": 9,
+	"token": "4f1c860728df71.38499022",
+
+	"info": {//send
+		"fName": "Sean",
+		"lName": "Lang",
+		"team": 2062
+	},
+
+	"prefs": {//send, optional info
+		"fade": true,
+		"verbose": true
+	},
+
+	"account": {//not sent, required info
+		"pword": "superpass",
+		"email": "slang800@gmail.com"
+	},
+
+	"stats": {//not sent, created by server side
+		"ip": "127.0.0.1",
+		"logintime": "1327269383.167"
+	}
+	
+	"opt": {//not sent, optional info
+		"zip": 53072,
+		"browser": "Firefox",
+		"gender": "m"
+	},
+}
+
+*/
 ?>
