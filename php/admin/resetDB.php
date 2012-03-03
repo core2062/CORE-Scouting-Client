346b74708519a0ad22d71f96b72b4a9c51e973ef
sean <slang800@gmail.com>
2012-03-03 15:08:05 -0600

    new file

---
+++ php/admin/resetDB.php
@@ -0,0 +1,69 @@
+<?php
+
+	//general
+	$db->createCollection("user");
+
+	$db->user->insert(
+		array(
+			"_id"=> "admin",
+			"permission"=> 9,
+			"token"=> "",
+
+			"info"=> array(
+				"fName"=> "Sean",
+				"lName"=> "Lang",
+				"team"=> 2062
+			),
+
+			"prefs"=> array(
+				"fade"=> true,
+				"verbose"=> true
+			),
+
+			"account"=> array(
+				"pword"=> "superpass",
+				"email"=> "slang800@gmail.com"
+			),
+
+			"stats"=> array(
+				"ip"=> "",
+				"logintime"=> 0
+			),
+
+			"opt"=> array(
+				"zip"=> 0,
+				"browser"=> "Firefox",
+				"gender"=> "m"
+			)
+		)
+	);
+
+	$db->createCollection("log");
+	$db->createCollection("globalVar");
+
+	$db->globalVar->insert(
+		array(
+			"_id" => "since_id",//for updateFMS (used while interacting with twitter)
+			"value" => 1
+		)
+	);
+
+	$db->globalVar->insert(
+		array(
+			"_id" => "devMode",//sets global devMode, if true, will override local devMode (in index.php)
+			"value" => false
+		)
+	);
+
+
+	//compiled collections
+	$db->createCollection("compiledEvent");
+	$db->createCollection("compiledTeam");
+
+
+	//source collections
+	$db->createCollection("sourceScouting");
+	$db->createCollection("sourceFMS");
+	$db->createCollection("sourceTeamInfo");
+	$db->createCollection("sourceEventInfo");
+?>
