666b3ae6601065ed53de2693a2d851a7d741f57f
Sean Lang <slang800@gmail.com>
2012-03-06 18:48:44 -0600

    it can submit data now

--- php/admin.php
+++ php/admin.php
@@ -263,7 +263,11 @@ case "getEvents": //get all events & add links for teams in each match (which wi
 
 break;
 case "updateFMS": //update scores/schedule of current or recent events (uses twitter)
+	$db->sourceFMS->remove(array());//clear out sourceFMS
+
 	$since_id = globalVar('since_id');
+	$since_id = 132931435354005504;
+
 	$fmsFiltered = array();
 
 	for($i = 1; $i < 17; $i++){
@@ -271,9 +275,9 @@ case "updateFMS": //update scores/schedule of current or recent events (uses twi
 
 		logger('getting page ' . $i);
 
-		logger($filename);
 		logger(file_exists($filename));
 		logger($vars['devMode']);
+		logger($since_id);
 
 		if(file_exists($filename) && $vars['devMode']){//check if page is in cache (caching is disabled if not in dev mode)
 			logger('getting from cache');
@@ -282,7 +286,9 @@ case "updateFMS": //update scores/schedule of current or recent events (uses twi
 		} else {
 			logger('getting from twitter');
 
-			$fmsFeed = file_get_contents('http://twitter.com/statuses/user_timeline/frcfms.json?since_id=' . $since_id);
+			$fmsFeed = file_get_contents('http://twitter.com/statuses/user_timeline/frcfms.json?trim_user=true&count=200&since_id=' . $since_id . "&page=" . $i, false);
+
+			logger($fmsFeed);
 
 			if($vars['devMode']){
 				$fp = fopen($filename, "w+");
@@ -298,56 +304,65 @@ case "updateFMS": //update scores/schedule of current or recent events (uses twi
 
 		$fmsFeed = json_decode($fmsFeed, true);
 
-		fb($fmsFeed);
-
 		foreach($fmsFeed as $obj){//$i = 0; $i > $len; $i++
-			$fmsFiltered[] = $obj['text'];
+
+			preg_match('/#FRC([^\s]*)/', $obj['text'], $info['eventCode']);
+			preg_match('/TY (P|Q|E)/', $obj['text'], $info['matchType']);
+			preg_match('/MC ([0-9]*)/', $obj['text'], $info['matchNumber']);
+
+			preg_match('/RF ([0-9]*)/', $obj['text'], $info['redFinalScore']);
+			preg_match('/BF ([0-9]*)/', $obj['text'], $info['blueFinalScore']);
+
+			preg_match('/RA ([0-9]*) ([0-9]*) ([0-9]*)/', $obj['text'], $info['redTeams']);
+			$info['redTeams'][1] = array((int)$info['redTeams'][1], (int)$info['redTeams'][2], (int)$info['redTeams'][3]);
+
+			preg_match('/BA ([0-9]*) ([0-9]*) ([0-9]*)/', $obj['text'], $info['blueTeams']);
+			$info['blueTeams'][1] = array((int)$info['blueTeams'][1], (int)$info['blueTeams'][2], (int)$info['blueTeams'][3]);
+
+			preg_match('/RB ([0-9]*)/', $obj['text'], $info['redBridgePoints']);
+			preg_match('/BB ([0-9]*)/', $obj['text'], $info['blueBridgePoints']);
+
+			preg_match('/RFP ([0-9]*)/', $obj['text'], $info['redFoulPoints']);
+			preg_match('/BFP ([0-9]*)/', $obj['text'], $info['blueFoulPoints']);
+
+			preg_match('/RHS ([0-9]*)/', $obj['text'], $info['redHybridBasketPoints']);
+			preg_match('/BHS ([0-9]*)/', $obj['text'], $info['blueHybridBasketPoints']);
+
+			preg_match('/RTS ([0-9]*)/', $obj['text'], $info['redTeleopBasketPoints']);
+			preg_match('/BTS ([0-9]*)/', $obj['text'], $info['blueTeleopBasketPoints']);
+
+			preg_match('/CS (0|1|2)/', $obj['text'], $info['coopertitionPoints']);
+
+			foreach ($info as $key => $value) {
+				if(!empty($info[$key][1])){//fixes issue with undefined index... happens becasuse one of the above wasn't found
+					$info[$key] = $info[$key][1];
+				} else {
+					$info[$key] = '';
+				}
+			}
+
+			$db->sourceFMS->update(
+				array(
+					"_id" => $obj['id_str']
+				),
+				array(
+					'$set' => array(
+						'text' => $obj['text'],
+						'data' => $info
+					)
+				),
+				true
+			);
 
 			if($obj['id'] > $since_id){
-				$since_id = $obj['id'];
+				$new_since_id = $obj['id_str'];
 			}
 		}
 	}
 
-	$len = count($fmsFiltered);
-	for($i = 0; $i < $len; $i++){
-		//#FRCArchimedes TY Q MC 100 RF 320 BF 410 RA 1234 5678 8900 BA 1234 5678 8900 RB 10 BB 10 RFP 0 BFP 0 RHS 0 BHS 0 RTS 0 BTS 0 CP 0
-		preg_match('/#FRC([^\s]*)/', $fmsFiltered[$i], $info['eventCode']);
-		preg_match('/TY (.)/', $fmsFiltered[$i], $info['matchType']);
-		preg_match('/MC ([0-9]*)/', $fmsFiltered[$i], $info['matchNumber']);
+	$since_id = 1;//remove later
 
-		preg_match('/RF ([0-9]*)/', $fmsFiltered[$i], $info['redFinalScore']);
-		preg_match('/BF ([0-9]*)/', $fmsFiltered[$i], $info['blueFinalScore']);
-		preg_match('/RF ([0-9]*)/', $fmsFiltered[$i], $info['matchNumber']);
-
-
-		foreach ($info as $key => $value) {
-			if(!empty($info[$key][1])){//fixes issue with undefined index... happens becasuse one of the above wasn't found
-				$info[$key] = $info[$key][1];
-			} else {
-				$info[$key] = '';
-			}
-		}
-
-		$db->sourceTeamInfo->update(
-			array(
-				"_id" => $obj['id']
-			),
-			array(
-				'$set' => array(
-					'data' => $info
-				)
-			),
-			true
-		);
-
-	}
-
-	$since_id = 0;//remove later
-		
-	globalVar('since_id', $since_id);
-
-	fb($fmsFiltered);
+	globalVar('since_id', $new_since_id);
 
 	send_reg(array('message' => 'finished updating FMS'));
 break;
--- php/input.php
+++ php/input.php
@@ -8,8 +8,10 @@ if(empty($input['inputType']) == true){
 	send_error('inputType was not sent');
 }
 
+$input['data']['inputType'] = $input['inputType'];
+
 //for all types of input
-$insert['meta'] = array(
+$input['data']['meta'] = array(
 	'scoutid' => $user['_id'],
 	'time' => $starttime,
 	'type' => $input['inputType'],
@@ -17,63 +19,37 @@ $insert['meta'] = array(
 );
 
 fb($input['data']);
-fb($insert);
 
 //for robot & alliance
 if($input['inputType'] == 'robot' || $input['inputType'] == 'alliance'){
 	if(empty($input['data']['matchNum'])){
 		send_error("match number was not correct");
 	}
-
-	$insert['_id'] = $input['data']['matchNum'];
 }
 
-switch ($input['inputType']) {
-case "robot":
+//validation of all data... log invalid data & change errorCount based on it
+//if error count is too high then set use = false
 
-	//validation of all data... log invalid data & change errorCount based on it
-	//if error count is too high then set use = false
-	
-	$db->raw->insert(
-		array_merge($insert, $input);
-	);
+$db->sourceScouting->insert(
+	$input['data']
+);
 
-	send_reg(array('message' => 'data submitted sucessfully'));
+send_reg(array('message' => 'data submitted sucessfully'));
 
-	/* Data Model
-	
-	_id: random number
-	meta: {
-		scoutid: scoutid of user
-		time: when data was entered
-		type: inputType (robot, alliance, or pit)
-		use: default as true, if error count is too high then it is false
-	}
-	sort: {
-		matchNum:
-	}
+/* Data Model
 
-
-	*/
-
-	
-break;
-case "alliance":
-	
-	//validation of all data... log invalid data & change errorCount based on it
-	//if error count is too high then set use = false
-	
-break;
-case "pit":
-	
-	//validation of all data... log invalid data & change errorCount based on it
-	//if error count is too high then set use = false
-	
-break;
-default:
-	send_error('invalid inputType','');
+_id: random number
+meta: {
+	scoutid: scoutid of user
+	time: when data was entered
+	type: inputType (robot, alliance, or pit)
+	use: default as true, if error count is too high then it is false
+}
+sort: {
+	matchNum:
 }
 
-$db->execute("db.raw.insert($insert)");
+*/
+
 
 ?>
