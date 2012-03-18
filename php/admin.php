f9e8c1b654cad58b2a745ce1ded6c1f45a8e212b
Sean Lang <slang800@gmail.com>
2012-03-17 23:31:07 -0500

    seperated stuff into new files & a bunch of other stuff & trying to find code I lost from OPR

--- php/admin.php
+++ php/admin.php
@@ -30,231 +30,7 @@ if($input['subRequest'] == 'getTeams' || $input['subRequest'] == 'getTeamProfile
 switch ($input['subRequest']) {
 case "getTeams": //gets the number & tpid (used by FIRST to identify teams) for each team, then gets all profiles
 
-	$db->sourceTeamInfo->remove([]);//clear out sourceTeamInfo
-
-	//get file to find number of teams
-	$contents = file_get_contents("https://my.usfirst.org/myarea/index.lasso?page=searchresults&skip_teams=0&programs=FRC&season_FRC=" . $year . "&reports=teams&results_size=25", false);
-
-	//find out how many teams were returned (to determine # of pages)
-	preg_match('/All Areas\s+\((....|...|..|.) found,/', $contents, $matches);
-	$totalTeamsFound = $matches[1];
-
-	function processTPID($input){//add each team to DB
-		global $db;
-
-		$db->sourceTeamInfo->update(
-			[
-				"_id" => (int)$input[2]
-			],
-			[
-				'$set' => [
-					"meta" => [
-						"tpid" => (int)$input[1]
-					]
-				]
-			],
-			true
-		);
-		return "";//is this needed or assumed????? & in below
-	}
-
-	//get team list pages from the FIRST site 
-	for($i = 0; $i < ($totalTeamsFound/250); $i++){
-		$filename = "tmp/db/getTPIDs/" . $year . "-" . $i;
-	
-		if(file_exists($filename) && $vars['devMode']){//check if page is in cache (caching is disabled if not in dev mode)
-			$contents = file_get_contents($filename);
-		} else {
-			$url = "https://my.usfirst.org/myarea/index.lasso?page=searchresults&results_size=250&omit_searchform=1&skip_teams=" . $i*250 . "&-session=myarea:" . $sessionID . "#FRC_teams";
-			$contents = file_get_contents($url, false);//consider replacing with cURL
-
-			//remove crap from files
-			//TODO: test if below improves speed
-			$contents = preg_replace('/\s+/', ' ',$contents, -1, $replacements); //removes double spaces, indents, and line breaks
-
-			//TODO: find fix and remove the 2 below regex... adding "s" to 2nd regex uses too much memory and = seg fault 
-			$contents = preg_replace('/<\/a>((?!<a).)*<a/', "</a><a", $contents, -1, $replacements); //removes other crap... another good one: '/<(\/)?t[^<>]*>/'
-			$contents = preg_replace('/&amp;-session=myarea:'. $sessionID . '"/', "", $contents, -1, $replacements);
-			//&amp;-session=myarea:C77D640507604078D1OjXUqD64EA"
-
-			$contents = preg_replace('/\s+/', ' ',$contents, -1, $replacements); //removes double spaces, indents, and line breaks
-
-
-			if($vars['devMode']){
-				$fp = fopen($filename, "w+");
-				fwrite($fp, $contents);
-				fclose($fp);
-			}
-		}
-
-		//question: is it better to run a regex on several small files of one big one
-		$regex = '/<a href="\?page=team_details&tpid=(.....)><b>(....|...|..|.)<\/b><\/a>/';
-		preg_replace_callback($regex, "processTPID", $contents);
-	}
-
-	logger('finished getting the number and tpid of each team');
-
-
-
-	//team profile getter
-
-	ini_set('max_execution_time', 120); //this script needs extra time
-
-
-	//read TPIDs that were just processed
-	$cursor = $db->sourceTeamInfo->find()->sort(["_id" => 1]);
-
-	function processEvent($input){//add each team to DB
-		global $db;
-		global $obj;
-
-		unset($input[0]);//this isn't needed
-		$input = array_values($input);//reset keys in array
-
-		//decode html characters
-		$len = count($input);
-		for ($i=1; $i < $len; $i++) {// first value is year of competition -- no special characters
-			$input[$i] = html_entity_decode($input[$i]);
-		}
-
-		$input['awards'] = [];
-
-		//split awards string into array
-		for ($i=2; $i < $len; $i++) {// award list starts at 2nd array element
-			$input['awards'] = (array)explode("<br />", $input[$i]) + $input['awards'];
-			unset($input[$i]);
-		}
-
-		//search for blank values in awards array & remove extra whitespace
-		$len = count($input['awards']);
-		for ($i=0; $i < $len; $i++) {
-			$input['awards'][$i] = trim($input['awards'][$i]);
-			if(empty($input['awards'][$i]) || $input['awards'][$i] == '<i>(2000 and prior award listings may be incomplete)</i>'){
-				unset($input['awards'][$i]);
-			}
-		}
-
-		$input['awards'] = array_values($input['awards']);//reset keys in array
-
-		$input[1] = str_replace($input[0] . ' ', '', $input[1]);//remove year prefix
-
-		//finally add to DB
-		//TODO: find faster way to do this (w/out copying and re-adding)
-		$events = $db->sourceTeamInfo->find(
-			[
-				'_id' => $obj['_id']
-			],
-			[
-				'events' => 1
-			]
-		);
-
-		$events = iterator_to_array($events);
-
-		if(!empty($events[$obj['_id']]['events'])){
-			$events = $events[$obj['_id']]['events'];//really only need this part
-		} else {
-			$events = [];//if the team has no events entered yet
-		}
-		
-		//TODO: make year be integer
-
-		$events[(int)$input[0]][$input[1]]['awards'] = $input['awards'];
-
-		$db->sourceTeamInfo->update(
-			[
-				"_id" => $obj['_id']
-			],
-			[
-				'$set' => [
-					"events" => $events
-				]
-			],
-			true
-		);
-		return "";
-	}
-
-	$count = 0;//after x teams, get new sessionID
-
-	//TODO: switch to seperate processes to built cache of pages to process & then extract data (low priority)
-	foreach($cursor as $obj){
-
-		logger('getting team:' . $obj['_id']);
-
-		$filename = "tmp/db/getTeamProfiles/" . $obj['_id'];
-
-		if(file_exists($filename) && $vars['devMode']){//check if page is in cache (caching is disabled if not in dev mode)
-			$contents = file_get_contents($filename);
-		} else {
-			if($count > 200){
-				getSessionID();
-				$count = 0;
-			}
-			$count++;
-
-			$url = "https://my.usfirst.org/myarea/index.lasso?page=team_details&tpid=" . $obj['meta']['tpid'] . "&-session=myarea:" . $sessionID;
-			//TODO: add way to re-try if connection times out 
-			$contents = file_get_contents($url, false);//consider replacing with cURL
-
-			//remove crap from files
-			//TODO: test if below improves speed
-			$contents = preg_replace('/(?:(v)?align="[a-z]*"|nowrap|bgcolor="#......"|width="..(?:.)?%"|<!--(.|\s)*?-->)/', '', $contents); //removes comments, and other crap
-			$contents = preg_replace('/\s+/', ' ',$contents); //removes spaces
-
-			if($vars['devMode']){
-				$fp = fopen($filename, "w+");
-				fwrite($fp, $contents);
-				fclose($fp);
-			}
-		}
-
-		//get basic team info
-		preg_match("/<td >Team Name<\/td> <td>([^<>]*)<\/td>/", $contents, $team['name']);
-		preg_match("/<td >Team Location<\/td> <td>([^<>]*)<\/td>/", $contents, $team['location']);
-		preg_match("/<td >Rookie Season<\/td> <td>(....)<\/td>/", $contents, $team['rookieYear']);
-		preg_match("/<td >Team Nickname<\/td> <td>([^<>]*)<\/td>/", $contents, $team['nickname']);
-		preg_match("/<td >Team Motto<\/td> <td>([^<>]*)<\/td>/", $contents, $team['motto']);
-		preg_match("/<td >Team Website<\/td> <td><a(?:[^>]*)?>([^<>]*)<\/a><\/td>/", $contents, $team['site']);
-		
-		//TODO: change to using a single object that gets inserted - low priority
-
-		//all those pregs had 1 backreferance, this moves the matches to proper place in array
-		//also decode them
-		foreach ($team as $key => $value) {
-
-			if(!empty($team[$key][1])){//fixes issue with undefined index... happens becasuse one of the above wasn't found
-				$team[$key] = $team[$key][1];
-			} else {
-				$team[$key] = '';
-			}
-			
-		}
-
-		settype($team['rookieYear'], "int");
-
-		$team['name'] = utf8_encode(html_entity_decode($team['name']));
-		$team['location'] = utf8_encode(html_entity_decode($team['location']));
-		$team['nickname'] = utf8_encode(html_entity_decode($team['nickname']));
-		$team['motto'] = utf8_encode(html_entity_decode($team['motto']));
-
-		//insert basic data
-		$db->sourceTeamInfo->update(
-			[
-				"_id" => $obj['_id']
-			],
-			[
-				'$set' => [
-					'info' => $team
-				]
-			],
-			true
-		);
-
-		//get events & input them
-		preg_replace_callback("/<tr > <td >([^<>]*)<\/td> <td >([^<>]*)<\/td> <td >((?:[^<>]*|<br \/>|<(?:\/)?i>)*)<\/td> <\/tr>/", "processEvent", $contents);
-	}
-
+	require_once 'php/admin/getTeams';
 	send_reg(['message' => 'finished getting team info']);
 
 break;
@@ -366,256 +142,17 @@ case "updateFMS": //update scores/schedule of current or recent events (uses twi
 	send_reg(['message' => 'finished updating FMS']);
 break;
 case "compile": //clear out log collection in mongoDB
-	$db->compiledTeam->remove([]);//clear out compiledTeam
 	
-	require_once "php/math/Matrix.php";
-
-	function isIn($objectPoint, $containerPoint1, $containerPoint2){
-		//checks if object (a specific point) is in square container
-		if($containerPoint1[0] > $containerPoint2[0]){
-			//switch points to put highest in point 2
-			$tmp = $containerPoint1[0];
-			$containerPoint1[0] = $containerPoint2[0];
-			$containerPoint2[0] = $tmp;
-		}
-		if($containerPoint1[1] > $containerPoint2[1]){
-			//switch points to put highest in point 2
-			$tmp = $containerPoint1[1];
-			$containerPoint1[1] = $containerPoint2[1];
-			$containerPoint2[1] = $tmp;
-		}
-
-		if($objectPoint[0] > $containerPoint2[0] || $objectPoint[0] < $containerPoint1[0] || $objectPoint[1] > $containerPoint2[1] || $objectPoint[1] < $containerPoint1[1]){
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	$teams = [3061, 1625, 2957, 4238, 3754, 3312, 4181, 2470, 3840, 2472, 3261, 3263, 3883, 3056, 3036, 2512, 4009, 4230, 2220, 1816, 3828, 3054, 2499, 2518, 2977, 3755, 3747, 2526, 2177, 2500, 2538, 4217, 3102, 2052, 3276, 3122, 3788, 2845, 3294, 2264, 2169, 2530, 2846, 2574, 3018, 3740, 3267, 2491, 3846, 3839, 3367, 4228, 2175, 3130, 877, 876, 93, 3197, 2506, 4011, 4054, 1714, 2826, 3381, 2062];
-	$blackList = [4230];//make something to work with this
-	sort($teams);
-	/*     ((:?[0-9])?(:?[0-9])?(:?[0-9])?(:?[0-9])?)</a>(:?(?!41vwsY18B13D)(:?.|\n))*41vwsY18B13D">     */
-
-
-	//OPR Calculations
-
-	$teamMatchups = array_fill_keys($teams, 0);
-	$teamScores = array_fill_keys($teams, 0);
-
-	foreach ($teamMatchups as $key => $value) {
-		$teamMatchups[$key] = array_fill_keys($teams, 0);
-	}
-
-	$cursor = $db->sourceFMS->find();
-
-	foreach($cursor as $obj){
-		if($obj['data']['eventCode'] == "DMN"){//use check with mongo???
-			$obj = $obj['data'];//only part that is needed
-
-			for($e=0; $e < 3; $e++){
-				$teamMatchups[ $obj['blueTeams'][$e] ][ $obj['blueTeams'][$e] ]++;
-				$teamMatchups[ $obj['redTeams'][$e] ][ $obj['redTeams'][$e] ]++;
-
-				$teamScores[ $obj['blueTeams'][$e] ] = $teamScores[ $obj['blueTeams'][$e] ] + $obj['blueFinalScore'];
-				$teamScores[ $obj['redTeams'][$e] ] = $teamScores[ $obj['redTeams'][$e] ] + $obj['redFinalScore'];
-
-				for($i=0; $i < 3; $i++){
-					$teamMatchups[ $obj['blueTeams'][$e] ][ $obj['redTeams'][$i] ]++;
-					$teamMatchups[ $obj['redTeams'][$e] ][ $obj['blueTeams'][$i] ]++;
-				}
-			}
-		}
-	}
-
-	ksort($teamMatchups);
-	foreach($teamMatchups as $key => $value){
-		ksort($teamMatchups[$key]);
-	}
-
-	$teamMatchups = array_values($teamMatchups);
-	foreach($teamMatchups as $key => $value){
-		$teamMatchups[$key] = array_values($teamMatchups[$key]);
-	}
-
-	$teamScores = array_values($teamScores);
-
-	//fb($teamMatchups);
-	//fb($teamScores);
-
-	$teamMatchupsMatrix = new Math_Matrix($teamMatchups);
-	$teamScoresVector = new Math_Vector($teamScores);
-
-	$oprValues = Math_Matrix::solve($teamMatchupsMatrix, $teamScoresVector);
-	$oprValues = $oprValues->_tuple->data;
-
-	$len = count($oprValues);
-	for($i = 0; $i < $len; $i++){
-		//$opr[] = [$teams[$i], $oprValues[$i]];
-		$opr[$teams[$i]] = $oprValues[$i];
-	}
-	unset($oprValues);
-/*
-	usort($opr, function (array $a, array $b){ return $a[1] - $b[1]; });
-	
-	$oprText = "";
-	foreach ($opr as $key => $value) {
-		$oprText .= '\n' . $value[0] . '=' . $value[1] . "\n"; 
-	}
-	fb($oprText);
-*/
-
-	//stats
-
-	$cursor = $db->sourceTeamInfo->find();
-
-	foreach($cursor as $obj){
-
-		//edit $obj here & add analysis stuff
-
-		$obj['opr'] = $opr[$obj["_id"]];//add opr data to team object
-
-		unset($obj['events']);//temporary
-
-		if(in_array($obj["_id"], $teams)){
-			$db->compiledTeam->insert($obj);
-		}
-	}
-
-
-	//process tracking info
-	$cursor = $db->sourceScouting->find(
-		[
-			'inputType' => 'tracking'
-		]
-	);
-
-	foreach($cursor as $obj){
-		if($obj['meta']['use']){
-
-			//get team object (to add data from previous matches together)
-			$currentTeam = $db->compiledTeam->findOne(
-				[
-					'_id' => (int)$obj['teamNum']
-				]
-			);
-
-			//get comments
-			$obj['comments'] = split("\n", $obj['comments']);
-
-			$currentTeam['matches'][$obj['matchNum']] = [
-				'comments' => $obj['comments']
-			];
-
-			$currentTeam['totalMatches']++;
-
-			//process tracking inputs
-			$len = count($obj['trackingInputs']);
-			for ($i=0; $i < $len; $i++) {
-				$currentObj = $obj['trackingInputs'][$i];//only need this stuff
-
-				//get shoot locations
-				$currentCoords = [ $currentObj['xCoord'] , $currentObj['yCoord'] ];
-
-				//check if it is from key
-				if(isIn($currentCoords, [0,53], [89,100]) || isIn($currentCoords, [213,53], [300,100])){
-					$location = 'key';
-				} else if(isIn($currentCoords, [0,150], [150,129]) || isIn($currentCoords, [300,0], [150,24])){
-					$location = 'fender';
-				} else {
-					$location = 'unspecified';
-				}
-				
-				if(isIn($currentCoords, [0,0], [150,150])){
-					$side = 'b';//based on color of key
-				} else {
-					$side = 'r';
-				}
-
-				if(($side == 'b' && $obj['allianceColor'] == 'r') || ($side == 'r' && $obj['allianceColor'] == 'b')){
-					if($location == 'fender'){
-						$side = 'same';
-					} else {
-						$side = 'opposite';
-					}
-				} else {
-					if($location == 'fender'){
-						$side = 'opposite';
-					} else {
-						$side = 'same';
-					}
-				}
-
-				$location = $side . ' Side, ' . $location;
-
-				$distanceFromHoopY = abs($currentObj['yCoord']-75);
-				if($obj['allianceColor'] == 'r'){
-					$distanceFromHoopX = $currentObj['xCoord'];
-				} else {
-					$distanceFromHoopX = 300-$currentObj['xCoord'];
-				}
-
-				$distanceFromHoop = sqrt($distanceFromHoopX^2 + $distanceFromHoopY^2)/sqrt(300^2 + 75^2);//0 is farthest you can get, 1 is right on top of the hoop (it's a percent)
-
-
-				$currentTeam['shooting']['totalShots']++;
-				$currentTeam['matches'][ $obj['matchNum'] ]['shooting']['totalShots']++;
-
-				//add distance info to object
-				$currentTeam['matches'][ $obj['matchNum'] ]['shooting']['shots'][] = [
-					'result' => empty($currentObj['score']) ? 'missed' : $currentObj['score'],
-					'distance' => $distanceFromHoop,
-					'place' => $location
-				];
-
-				//add to shoot totals
-				if(!empty($currentObj['score'])){
-					$currentTeam['shooting']['totalScores']++;
-					$currentTeam['matches'][ $obj['matchNum'] ]['shooting']['totalScores']++;
-
-					//increase score total for the correct hoop
-					$currentTeam['shooting']['heightTotal'][ $currentObj['score'] ]++;
-					$currentTeam['matches'][ $obj['matchNum'] ]['shooting']['heightTotal'][ $currentObj['score'] ]++;
-				}
-
-				//more stuff here
-			}
-
-			//get averages
-/*
-			foreach($currentTeam['matches'] as $key => $value) {
-				$currentTeam['matches'][$key]['shooting'][''];
-			}//NOT FINISHED HERE!!!!!!!!!!!!!!!!!
-*/
-
-
-			//check for incorrect teamNum (not fatal if exists, just wrong)
-			if(!in_array($obj["teamNum"], $teams)){
-				logger('wrong teamNum in match ' . $obj["matchNum"] . ' : ' . $obj["teamNum"], true);
-			}
-
-			//write new data to team object
-			$db->compiledTeam->remove(
-				[
-					'_id' => (int)$obj['teamNum']
-				]
-			);//remove old one
-			$db->compiledTeam->insert($currentTeam);//insert new one
-
-			
-		}
-	}
-
+	require_once "php/admin/compile.php";
 	send_reg(['message' => 'db is compiled']);
 
 break;
 case "export":
 
 	$cursor = $db->compiledTeam->find();
-
 	foreach ($cursor as $obj) {
 		$csv[] = [
-			'teamNum' => 0,
+			'teamNum' => $obj['_id'],
 			'matches' => 0,
 			'shots' => 0,
 			'scores' => 0,
@@ -623,7 +160,7 @@ case "export":
 			'middleBasketScores' => 0,
 			'bottomBasketScores' => 0,
 			'balenceAttempts' => 0,
-			//number of time bridge was brought down (just indicates ability to bring bridge down, not necessarily balancing)
+			//number of times bridge was brought down (just indicates ability to bring bridge down, not necessarily balancing)
 			'sucessfulBalence' => 0,//total of below 3 
 			'1RobotBalence' => 0,
 			'2RobotBalence' => 0,
@@ -661,13 +198,13 @@ case "export":
 	fwrite($fp, $contents);
 	fclose($fp);
 
-	globalVar('exportTotal', globalVar('exportTotal')++;);
-
-	send_reg(['message' => 'export is avaliable in /tmp/exports']);
+	send_reg(['message' => 'export is avaliable in /tmp/db/exports']);
 
 break;
 case "clearTmp": //clear out & rebuild tmp
-
+	
+	clearTmp();
+	send_reg(['message' => 'tmp directory is rebuilt']);
 
 break;
 case "clearLog": //clear out log collection in mongoDB
@@ -678,6 +215,7 @@ case "clearLog": //clear out log collection in mongoDB
 break;
 case "resetDB": //make all the collections / vars needed for the site and remove current
 
+	send_error('bad idea, and not finished');
 	//TODO: finish & add stuff to empty db (and maybe export into a backup in temp)
 	require_once "php/admin/resetDB.php";
 
@@ -685,6 +223,7 @@ break;
 case "backupDB": //copy DB to file in tmp/backup
 
 	//TODO: finish
+	send_error('not finished');
 
 break;
 default:
@@ -709,16 +248,6 @@ function clearTmp(){
 	mkdir($cwd . "/tmp/db/getTeamProfiles");
 	mkdir($cwd . "/tmp/db/getEvents");
 	mkdir($cwd . "/tmp/db/updateFMS");
-
-	globalVar('exportTotal', 0);
-
-	send_reg(['message' => 'tmp directory is rebuilt']);
+	mkdir($cwd . "/tmp/db/export");
 }
-
-
-/*
-Errorcheck
-Recalculate
-Reset DB
-*/
 ?>
