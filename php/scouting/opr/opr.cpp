cec051d6d684fd879910388a9dd0cc2ab64d0a22
sean <slang800@gmail.com>
2012-03-31 13:54:53 -0500

    removed stuff for opr in c++... going to do it in py

--- php/scouting/opr/opr.cpp
+++
@@ -1,8 +0,0 @@
-// this script is used to calculate opr, becasue php is not powerful enough to do it
-
-#include <iostream>
-
-int main (){
-	std::cout << "I should make some code for opr." << std::endl;
-	return 0;
-}
