4e940a9830cc070e3288d99367a5d872a3b3bae4
sean <slang800@gmail.com>
2012-04-14 01:42:24 -0500

    added php string parser

--- php/base.php
+++ php/base.php
@@ -141,4 +141,39 @@ function array_add($arrays){//parameter (arrays) is a array of the arrays to be 
 	return $sum;
 }
 
+class PhpStringParser {
+	protected $variables;
+
+	public function __construct($variables = array()){
+		$this->variables = $variables;
+	}
+
+	protected function eval_block($matches){
+		if( is_array($this->variables) && count($this->variables) ){
+			foreach($this->variables as $var_name => $var_value){
+				$$var_name = $var_value;
+			}
+		}
+
+		$eval_end = '';
+
+		if( $matches[1] == '<?=' || $matches[1] == '<?php=' ){
+			if( $matches[2][count($matches[2]-1)] !== ';' ){
+				$eval_end = ';';
+			}
+		}
+
+		$return_block = '';
+
+		eval('$return_block = ' . $matches[2] . $eval_end);
+
+		return $return_block;
+	}
+
+	public function parse($string){
+		return preg_replace_callback('/(\<\?=|\<\?php=|\<\?php)(.*?)\?\>/', array(&$this, 'eval_block'), $string);
+	}
+}
+
+
 ?>
