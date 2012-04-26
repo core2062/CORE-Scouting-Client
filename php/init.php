d4342545358fd2d0c695fb0683b75bb9ddadc892
Sean Lang <slang800@gmail.com>
2012-04-26 17:41:39 -0500

    added notice about rebuild tmp files & added temporary filtering to remove non-St. Louis data & added todo

--- php/init.php
+++ php/init.php
@@ -5,8 +5,8 @@
 list($micro, $sec) = explode(" ",microtime());
 $starttime = (float)$sec + (float)$micro;
 
-//dependencies
-if (!version_compare(PHP_VERSION, "5.4")) {//TODO: test php version check
+//dependencies & required files
+if (version_compare(PHP_VERSION, '5.4') != 1) {
 	die("you're gonna need php 5.4 to run this, sorry");
 }
 if (!class_exists("Mongo")) {
@@ -38,6 +38,12 @@ if(globalVar('devMode')){
 
 	error_reporting( E_ALL );
 	ini_set('display_errors', 1);
+}
+
+if(!file_exists('tmp')){
+	//NOTICE: if there are errors with tmp files, the entire directory should be removed (because this code will cause it to be rebuilt correctly)
+	require 'php/maintenance/maintenance.php';
+	clearTmp();
 }
 
 //get basic variables
