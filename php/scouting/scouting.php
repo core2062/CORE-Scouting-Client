5d35ee58840b7cdca4b280d6360b85194ea208dd
Sean Lang <slang800@gmail.com>
2012-04-29 11:58:30 -0500

    removed old code

--- php/scouting/scouting.php
+++ php/scouting/scouting.php
@@ -2,7 +2,6 @@
 
 //get teams from event db
 $teams = globalVar('current_teams');
-//$teams = array_diff($teams, globalVar('blacklist')); //probably shouldn't use blacklist here
 sort($teams);
 /*     ((:?[0-9])?(:?[0-9])?(:?[0-9])?(:?[0-9])?)</a>(:?(?!41vwsY18B13D)(:?.|\n))*41vwsY18echo">     */
 
