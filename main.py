20b1c154e726d04402fe6967763088e4d2b07799
Sean Lang <slang800@gmail.com>
2012-08-11 23:04:56 -0500

    working on evenLESS & Coffee Script compiler & commented out static-subdomain file server code

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 from flask import Flask, send_from_directory
-import config
-import helper
-import lessplus
+from config import STATIC_DIR
+from helper import compile_coffee
+import evenless
 
 app = Flask(__name__)
 
-# compile LESS & Coffee Script
-if app.debug:
-	helper.compile_less()
+#if app.debug:
+#	helper.compile_less()
 
 
 @app.route('/favicon.ico')
 def favicon():
 	"""send the favicon from the typical location at /favicon.ico"""
 	return send_from_directory(
-		config.CWD + 'static',  # static directory
+		STATIC_DIR,
 		'favicon.ico',
 		mimetype='image/vnd.microsoft.icon',
 		cache_timeout=60 * 60 * 24 * 365 * 5,  # set cache timeout to 5 years
 	)
 
 
-@app.route('/<path:filename>', subdomain="static")
-def static(filename):
-	"""send static files from separate sub-domain"""
-	return send_from_directory(
-		app.static_folder,
-		filename,
-	)
+# @app.route('/<path:filename>', subdomain="static")
+# def static(filename):
+# 	"""send static files from separate sub-domain"""
+# 	return send_from_directory(
+# 		app.static_folder,
+# 		filename,
+# 	)
 
 
 @app.route('/')
