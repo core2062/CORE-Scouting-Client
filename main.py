f6f09dbe6cc2b4f9e711384f9da8446b1345002c
Sean Lang <slang800@gmail.com>
2012-09-17 16:16:25 -0500

    pass debug mode from Flask to jade and use for determining compression of CSS

--- main.py
+++ main.py
@@ -30,7 +30,13 @@ def favicon():
 
 @app.route('/')
 def index():
-	return compile_jade('main.jade', pretty=app.debug)  # pretty is true if in debug
+	return compile_jade(
+		'main.jade',
+		pretty=app.debug,  # pretty is true if in debug
+		options={
+			'debug': app.debug
+		}
+	)
 
 if __name__ == "__main__":
 	app.run(
